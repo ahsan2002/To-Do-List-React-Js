d77a29a79b263de91ed8004acf153f0c3d0148d9
Ahsan <omerjee.ahsan@gmail.com>
2022-10-03 15:19:43 +0500

    in process

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import todo from "../images/todo.svg";
 import AddIcon from "@mui/icons-material/Add";
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
 import Button from '@mui/material/Button';
 
 const Todo = () => {
@@ -13,22 +14,23 @@ const Todo = () => {
         alert('Please enter your task')
     }
     else{
-    setitems([...items, inputdata]);
+      const myinput={ id:new Date().getTime().toString(), name:inputdata}
+    setitems([...items, myinput]);
     setinputdata("");
     }
   };
 
-  const DeleteItem=(id)=>{
-    const updateditem=items.filter((ele,ind)=>{
-        return ind !==id;
+  const DeleteItem=(index)=>{
+    const updateditem=items.filter((ele)=>{
+        return index !==ele.id;
     })
     setitems(updateditem);
 
   }
 
-  // const removeAll=()=>{
-  //   setitems([])
-  // }
+  const removeAll=()=>{
+    setitems([])
+  }
 
   return (
     <>
@@ -48,24 +50,25 @@ const Todo = () => {
                 setinputdata(e.target.value);
               }}
             />
-            <button className="fa" title="Add Item" onClick={addItem}>
+            <button className="fa" title="Add Task" onClick={addItem}>
               <AddIcon />
             </button>
           </div>
 
           <div className="show-Items">
-            {items.map((ele,ind)=>{
+            {items.map((ele)=>{
                 return(
-            <div className="eachItem">
-              <h3>{ele}</h3>
-              <button title="Delete Item" className="delbtn" onClick={()=>DeleteItem(ind)}><DeleteOutlineIcon/></button>
+            <div className="eachItem" key={ele.id}>
+              <h3>{ele.name}</h3>
+              <button title="Edit Task" className="editbtn"><ModeEditOutlineOutlinedIcon/></button>
+              <button title="Delete Task" className="delbtn" onClick={()=>DeleteItem(ele.id)}><DeleteOutlineIcon/></button>
             </div>
                 )
             })}
           </div>
 
           <div className="showItems">
-        <Button variant="contained">Remove All</Button>
+        <Button title="Remove All" onClick={removeAll} variant="contained">Remove All</Button>
           </div>
         </div>
       </div>
